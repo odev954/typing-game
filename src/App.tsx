80c2794731e18255c30d311ebc32d44cbfcdd6a1
odev954 <kleinbottle175@gmail.com>
2022-09-28 16:45:55 +0300

    starting work on type tracker component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App()
         <>
             <Clock />
             <ScoreRecorder />
-            <TypeTracker />
+            <TypeTracker Text={'test test test'} />
         </>
     )
 }
--- src/components/TypeTracker/TypeTracker.tsx
+++ src/components/TypeTracker/TypeTracker.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import useTrackerLogic from "src/hooks/useTrackerLogic/useTrackerLogic";
 import './TypeTracker.css'
 
-export default function TypeTracker() : JSX.Element
+interface TypeTrackerProps {
+    Text: string
+}
+
+export default function TypeTracker(props: TypeTrackerProps) : JSX.Element
 {
+    const [status, updateStatus] = useTrackerLogic(props.Text);
+    
+
+
     return (
-        <></>
+        <div>
+            { status.Words.slice(0, status.Position).map((word) => <p className='word completed'> word </p>)}
+            
+            <p className='word current'> { status.Words[status.Position] } </p>
+
+            { status.Words.slice(status.Position + 1).map((word) => <p className='word uncompleted'> word </p>)}
+        </div>
     )
 }
