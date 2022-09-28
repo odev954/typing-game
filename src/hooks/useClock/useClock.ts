2149b8cb6f7acb7aceb2825b1bb3e6e72a39fdb8
odev954 <kleinbottle175@gmail.com>
2022-09-28 17:00:29 +0300

    updating exports in use clock hook

--- src/hooks/useClock/useClock.ts
+++ src/hooks/useClock/useClock.ts
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const ONE_SECOND = 1000;
 
-function useClock(seconds=60, timeoutExceededCallback=null): number {
+export default function useClock(seconds=60, timeoutExceededCallback=null): number {
     const [remainingSeconds, updateRemainingSeconds] = useState<number>(seconds);
     const intervalId = setInterval(() => updateRemainingSeconds((seconds) => seconds - 1), ONE_SECOND);
     
