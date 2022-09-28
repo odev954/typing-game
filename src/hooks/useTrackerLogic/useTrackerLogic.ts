f78a7c20355ef4f3cb93fe4a6eb2db243ffb6a8b
odev954 <kleinbottle175@gmail.com>
2022-09-28 15:49:02 +0300

    adding use tracker logic hook

---
+++ src/hooks/useTrackerLogic/useTrackerLogic.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export interface TrackerStatus
+{
+    Words: string[];
+    Position: number;
+    TyposCount: number;
+}
+
+export default function useTrackerLogic(text: string) {
+    const [status, updateStatus] = useState<TrackerStatus>({ Words: text.split(' '), Position: 0, TyposCount: 0 });
+
+    return [
+        status,
+        (word : string) : boolean => {
+            let success : boolean = status.Words[status.Position] === word;  
+            
+            if(success)
+            {
+                updateStatus({ 
+                    Words: status.Words, 
+                    Position: status.Position + 1, 
+                    TyposCount: status.TyposCount 
+                })
+            }
+            else
+            {
+                updateStatus({ 
+                    Words: status.Words, 
+                    Position: status.Position, 
+                    TyposCount: status.TyposCount + 1 
+                })
+            }
+
+            return success;
+        }
+    ]
+}
