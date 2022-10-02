afdf5a58bb69107ff986d0c4e62504aef259bd9d
odev954 <kleinbottle175@gmail.com>
2022-10-02 11:24:48 +0300

    adding use effect to use clock hook

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import './Clock.css'
+import useClock from "src/hooks/useClock/useClock";
+import Card from 'react-bootstrap/Card';
+import './Clock.css';
 
 export default function Clock() : JSX.Element
 {
+    const timeoutSeconds = useClock(() => console.log('timeout!'));
+
     return (
-        <></>
+        <div>
+            <p>timeout: {Math.floor(timeoutSeconds / 60)}:{timeoutSeconds % 60}</p>
+        </div>
     )
 }
--- src/hooks/useClock/useClock.ts
+++ src/hooks/useClock/useClock.ts
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 const ONE_SECOND = 1000;
 
-export default function useClock(seconds=60, timeoutExceededCallback=null): number {
+export default function useClock(timeoutExceededCallback=null, seconds=60): number {
     const [remainingSeconds, updateRemainingSeconds] = useState<number>(seconds);
-    const intervalId = setInterval(() => updateRemainingSeconds((seconds) => seconds - 1), ONE_SECOND);
+    const callback = useCallback(() => {
+        updateRemainingSeconds((current) => current - 1)
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(callback, ONE_SECOND);
+
+        setTimeout(() => {
+            if(timeoutExceededCallback)
+            {
+                timeoutExceededCallback();
+            }
+            
+            clearInterval(intervalId);
+        }, ONE_SECOND * seconds);
+    }, []);
     
-    setTimeout(() => {
-        if(timeoutExceededCallback)
-        {
-            timeoutExceededCallback();
-        }
-        
-        clearInterval(intervalId);
-    }, ONE_SECOND * seconds);
 
     return remainingSeconds;
 }
