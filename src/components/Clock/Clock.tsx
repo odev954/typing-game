99483ad10572203e5b08303dd429d761c2890900
odev954 <kleinbottle175@gmail.com>
2022-10-02 12:09:09 +0300

    styling clock component

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import useClock from "src/hooks/useClock/useClock";
-import Card from 'react-bootstrap/Card';
+import Card from '@mui/material/Card';
 import './Clock.css';
+import { CardContent, Typography } from "@mui/material";
 
 export default function Clock() : JSX.Element
 {
     const timeoutSeconds = useClock(() => console.log('timeout!'));
 
     return (
-        <div>
-            <p>timeout: {Math.floor(timeoutSeconds / 60)}:{timeoutSeconds % 60}</p>
-        </div>
+        <Card className="clock display-card">
+            <CardContent>
+                <Typography className="clock styled-text">
+                    {Math.floor(timeoutSeconds / 60)}:{timeoutSeconds % 60 > 9 ? timeoutSeconds % 60 : '0' + timeoutSeconds % 60}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
