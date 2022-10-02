f705c467f74134ff694d61ed08cc7219c053c2dd
odev954 <kleinbottle175@gmail.com>
2022-10-02 13:37:23 +0300

    adding timeout alert

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import useClock from "src/hooks/useClock/useClock";
 import Card from '@mui/material/Card';
+import { Snackbar, CardContent, Typography } from "@mui/material";
 import './Clock.css';
-import { CardContent, Typography } from "@mui/material";
+import { Container } from "react-bootstrap";
 
 export default function Clock() : JSX.Element
 {
-    const timeoutSeconds = useClock(() => console.log('timeout!'));
+    const [alert, setAlert] = useState(false);
+    const timeoutSeconds = useClock(() => setAlert(_ => true), 3);
 
     return (
-        <Card className="clock display-card">
-            <CardContent>
-                <Typography className="clock styled-text">
-                    {Math.floor(timeoutSeconds / 60)}:{timeoutSeconds % 60 > 9 ? timeoutSeconds % 60 : '0' + timeoutSeconds % 60}
-                </Typography>
-            </CardContent>
-        </Card>
+        <Container>
+            <Snackbar open={alert} message='stop!'></Snackbar>
+            <Card className="clock display-card">
+                <CardContent>
+                    <Typography className="clock styled-text">
+                        {Math.floor(timeoutSeconds / 60)}:{timeoutSeconds % 60 > 9 ? timeoutSeconds % 60 : '0' + timeoutSeconds % 60}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Container>
     )
 }
