2f9b208509dc8ecc0ba884c2197b8df1003de52c
odev954 <kleinbottle175@gmail.com>
2022-10-06 09:51:22 +0300

    adding score calculation

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,30 +1,16 @@
 import React, { useCallback, useState } from "react";
 import useClock from "src/hooks/useClock/useClock";
 import Card from '@mui/material/Card';
-import { Backdrop, CardContent, Typography, Container, Button } from "@mui/material";
+import { CardContent, Typography, Container } from "@mui/material";
 import './Clock.css';
 
 export default function Clock() : JSX.Element
 {
-    const [alert, setAlert] = useState(false);
-    const timeoutSeconds = useClock(() => setAlert(_ => true), 3);
-    const handleClose = useCallback(() => {
-        setAlert(_ => false);
-    }, [alert])
+    const timeoutSeconds = useClock();
 
     return (
         <Container className="clock display-container">
-            <Backdrop className="clock finish-backdrop" 
-                open={alert} 
-                onClick={handleClose}>
-                <Card className="clock display-finish">
-                    <CardContent>
-                        <Typography className="clock styled-text">Timeout! Timeout! Timeout!</Typography>
-                        <Typography className="clock styled-text">Your score is: {100} w/min</Typography>
-                    </CardContent>
-                </Card>
-                <Button className="clock play-btn" variant="contained">play again</Button>
-            </Backdrop>
+            
             <Card className="clock display-card">
                 <CardContent>
                     <Typography className="clock styled-text">
--- src/components/TypeTracker/TypeTracker.tsx
+++ src/components/TypeTracker/TypeTracker.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback, useRef, useState } from "react";
 import useTrackerLogic from "src/hooks/useTrackerLogic/useTrackerLogic";
 import Grid from '@mui/material/grid';
-import { Typography, Card, CardContent, TextField, Snackbar, Alert } from "@mui/material";
+import { Typography, Card, CardContent, TextField, Snackbar, Alert, Backdrop, Button } from "@mui/material";
+import useClock from "src/hooks/useClock/useClock";
 import _ from 'lodash';
 import './TypeTracker.css';
 
@@ -11,11 +12,17 @@ interface TypeTrackerProps {
 
 export default function TypeTracker(props: TypeTrackerProps) : JSX.Element
 {
+    const [alert, setAlert] = useState(false);
     const [isTypo, updateIsTypo] = useState(false);
     const [status, updateTrackerStatus] = useTrackerLogic(props.Text);
+    const timeoutSeconds = useClock(() => setAlert(_ => true));
     const textInputRef = useRef(null);
     const WORDS_PER_ROW : number = 5;
-    let sections : string[][] = _.chunk(status.Words, WORDS_PER_ROW);   
+    let sections : string[][] = _.chunk(status.Words, WORDS_PER_ROW);  
+    
+    const handleClose = useCallback(() => {
+        setAlert(_ => false);
+    }, [alert]) 
     
     const inputHandler = useCallback(() => {
         let flag = false;
@@ -35,6 +42,17 @@ export default function TypeTracker(props: TypeTrackerProps) : JSX.Element
 
     return (
         <Card className="tracker display-card">
+            <Backdrop className="clock finish-backdrop" 
+                open={alert} 
+                onClick={handleClose}>
+                <Card className="clock display-finish">
+                    <CardContent>
+                        <Typography className="clock styled-text">Timeout! Timeout! Timeout!</Typography>
+                        <Typography className="clock styled-text">Your score is: {status.Position + 1} w/min</Typography>
+                    </CardContent>
+                </Card>
+                <Button className="clock play-btn" variant="contained">play again</Button>
+            </Backdrop>
             <Snackbar
                 anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
                 open={isTypo}
