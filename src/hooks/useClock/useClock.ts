eb2e5453dc15bf382cc11c6ba7093b6cb188f78b
orel aharonov <orel@invarial.com>
2022-10-09 10:52:24 +0300

    adding reset trigger to use clock hook

--- src/hooks/useClock/useClock.ts
+++ src/hooks/useClock/useClock.ts
@@ -2,24 +2,30 @@ import React, { useState, useCallback, useEffect } from "react";
 
 const ONE_SECOND = 1000;
 
-export default function useClock(timeoutExceededCallback=null, seconds=60): number {
-    const [remainingSeconds, updateRemainingSeconds] = useState<number>(seconds);
+export default function useClock(timeoutExceededCallback=null, resetTrigger=null, seconds=60): number {
+    const [remainingSeconds, updateRemainingSeconds] = useState<number>(0);
     const callback = useCallback(() => {
         updateRemainingSeconds((current) => current - 1)
     }, []);
 
     useEffect(() => {
-        const intervalId = setInterval(callback, ONE_SECOND);
+        let intervalId = null;
+        
+        if(resetTrigger)
+        {
+            intervalId = setInterval(callback, ONE_SECOND);
 
-        setTimeout(() => {
-            if(timeoutExceededCallback)
-            {
-                timeoutExceededCallback();
-            }
-            
-            clearInterval(intervalId);
-        }, ONE_SECOND * seconds);
-    }, []);
+            updateRemainingSeconds(_ => seconds)
+            setTimeout(() => {
+                if(timeoutExceededCallback)
+                {
+                    timeoutExceededCallback();
+                }
+                
+                clearInterval(intervalId);
+            }, ONE_SECOND * seconds);
+        }
+    }, [resetTrigger]);
     
 
     return remainingSeconds;
