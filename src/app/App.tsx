81f254ea20f9749f87ea687088dafe8ac35fa72b
orel aharonov <orel@invarial.com>
2022-10-09 10:54:32 +0300

    moving shared logic to app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Clock from '../components/Clock/Clock';
 import ScoreRecorder from "components/ScoreRecorder/ScoreRecorder";
 import TypeTracker from "components/TypeTracker/TypeTracker";
+import useStorage from "src/hooks/useStorage/useStorage";
+import useTrackerLogic from "src/hooks/useTrackerLogic/useTrackerLogic";
+import useClock from "src/hooks/useClock/useClock";
 import './styles.css';
-import useWords from "src/hooks/useWords/useWords";
+import FinishScreen from "components/FinishScreen/FinishScreen";
 
 export default function App() 
 {
-    const text = useWords();
+    const [resetClock, updateResetClock] = useState(true);
+    const [finishScreenOpen, setFinishScreenOpen] = useState(false);
+    const [status, updateTrackerStatus, resetTracker] = useTrackerLogic();
+    const [, updateScores] = useStorage();
+    const timeoutSeconds = useClock(() => {
+        setFinishScreenOpen(_ => true);
+        updateResetClock(_ => false);
+    }, resetClock);
+    const handleClose = useCallback(() => {
+        setFinishScreenOpen(_ => false);
+        updateScores({
+            date: new Date(Date.now()),
+            count: status.Position
+        });
+        resetTracker();
+        updateResetClock(_ => true);
+    }, [finishScreenOpen, status]) 
 
     return ( 
         <div className="container">
-            <Clock />
-            <TypeTracker/>
+            <FinishScreen Open={finishScreenOpen} Score={status.Position} OnClose={handleClose}/>
+            <Clock TimeoutSeconds={timeoutSeconds}/>
+            <TypeTracker UpdateTrackerStatus={updateTrackerStatus} Status={status}/>
             <ScoreRecorder />
         </div>
     )
